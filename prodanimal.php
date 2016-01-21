d67d79e123f8f874164ff15cf16a19b299ed4e4c
rlhardrock <rlhardrock@hotmail.com>
2016-01-20 22:57:45 -0500

    Create prodanimal.php

---
+++ prodanimal.php
@@ -0,0 +1,16 @@
+<?php
+  
+  //ttlprin    titulo principal
+  $ttlprin = $_GET['aaa']
+ 
+  //ttlsgn    titulo segungo nivel
+  $ttlsgn1 = $_GET['bbb']
+  $ttlsgn2 = $_GET['ccc']
+
+  //ttltcn    titulo tercer nivel
+  $ttltcn = $_GET['ddd']
+
+
+
+?>
+
