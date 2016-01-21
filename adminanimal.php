fa119bb95fc9d3ccd4cf07c400c642fdb403c2a9
rlhardrock <rlhardrock@hotmail.com>
2016-01-20 23:42:57 -0500

    Update adminanimal.php

--- adminanimal.php
+++ adminanimal.php
@@ -1,27 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Administracio</title>
+  <title>Administracions</title>
 </head>
   <body>
  <h2>ADMINISTRACION</h2>
+  
+  <form>
   <name="formulario" method="post" action="prodanimal.php">
    <br>
-  <h3>Titulo Principal:</h3>
+   
+    <h3>Titulo Principal:</h3>
     <input type="text" name="aaa"><br>
-    <input type="submit" value="Enviar" /> 
+    <input type="submit" value="Cambiar" /> 
    <br>
+    
      <h4>Titulos de Segundo Nivel:</h4>
       <p>Nombre de la Seccion 1</p>
          <input type="text" name="bbb"><br>
-         <input type="submit" value="Enviar" /> 
+         <input type="submit" value="Cambiar" /> 
        <br>
-      <p>Nombre de la Seccion 1</p>
+      <p>Nombre de la Seccion 2</p>
          <input type="text" name="ccc"><br>
-         <input type="submit" value="Enviar" />
+         <input type="submit" value="Cambiar" />
        <br>
+
       <h4>Titulo de Tercer Nivel:</h4>
          <input type="text" name="ddd"><br>
-         <input type="submit" value="Enviar" />
+         <input type="submit" value="Cambiar" />
+   </form>      
   </body>
 </html> 
