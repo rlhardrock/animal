1b84975a4be12cf3bb9c1a9b26227f051481ea1e
rlhardrock <rlhardrock@hotmail.com>
2016-01-20 22:57:14 -0500

    Create adminanimal.php

---
+++ adminanimal.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Administracio</title>
+</head>
+  <body>
+ <h2>ADMINISTRACION</h2>
+  <name="formulario" method="post" action="prodanimal.php">
+   <br>
+  <h3>Titulo Principal:</h3>
+    <input type="text" name="aaa"><br>
+    <input type="submit" value="Enviar" /> 
+   <br>
+     <h4>Titulos de Segundo Nivel:</h4>
+      <p>Nombre de la Seccion 1</p>
+         <input type="text" name="bbb"><br>
+         <input type="submit" value="Enviar" /> 
+       <br>
+      <p>Nombre de la Seccion 1</p>
+         <input type="text" name="ccc"><br>
+         <input type="submit" value="Enviar" />
+       <br>
+      <h4>Titulo de Tercer Nivel:</h4>
+         <input type="text" name="ddd"><br>
+         <input type="submit" value="Enviar" />
+  </body>
+</html> 
