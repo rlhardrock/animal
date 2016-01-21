157ffccefd5fffc6c3135c747e70dadac89916a6
rlhardrock <rlhardrock@hotmail.com>
2016-01-20 23:21:19 -0500

    Update prodanimal.php

--- prodanimal.php
+++ prodanimal.php
@@ -1,16 +1,13 @@
 <?php
-  
-  //ttlprin    titulo principal
-  $ttlprin = $_GET['aaa']
- 
-  //ttlsgn    titulo segungo nivel
-  $ttlsgn1 = $_GET['bbb']
-  $ttlsgn2 = $_GET['ccc']
-
-  //ttltcn    titulo tercer nivel
-  $ttltcn = $_GET['ddd']
-
-
-
+ //ttlprin   titulo principal
+  $ttlprin = $_POST['aaa'];
+ //ttlsgn    titulo segundo nivel
+  $ttlsgn1 = $_POST['bbb'];
+  $ttlsgn2 = $_POST['ccc'];
+ //ttltcn    titulo tercer nivel
+  $ttltcn = $_POST['ddd'];
+ //prueba de captura
+  echo "$ttlprin <br>";
+  echo "$ttlsgn1 <br>";
+  echo "$ttlsgn2 <br>";
 ?>
-
